9deacdef30f2d4d49072f9e69600bdecd3e54e95
AMacari <werxes@gmail.com>
2018-10-09 09:24:58 +0300

    init

---
+++ src/main/java/com/code/saraphan/SaraphanApplication.java
@@ -0,0 +1,13 @@
+package com.code.saraphan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SaraphanApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaraphanApplication.class, args);
+	}
+}

506d6a5bdf758672da2be24576857dbcaa0dd2c6
AMacari <werxes@gmail.com>
2018-11-26 12:58:51 +0200

    Vutefy

--- src/main/resources/static/js/util/ws.js
+++ src/main/resources/static/js/util/ws.js
@@ -8,13 +8,14 @@ const handlers = []
 export function connect() {
     const socket = new SockJS('/gs-guide-websocket')
     stompClient = Stomp.over(socket)
+    stompClient.debug = () => {}
     stompClient.connect({}, frame => {
-        console.log('Connected: ' + frame)
-    stompClient.subscribe('/topic/activity', message => {
+        stompClient.subscribe('/topic/activity', message => {
         handlers.forEach(handler => handler(JSON.parse(message.body)))
 })
 })
 }
+
 
 export function addHandler(handler) {
     handlers.push(handler)
